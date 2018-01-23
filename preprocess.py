413e26d1ad73a7082bc555a680aade0d19ae5494
Sidharth Ramesh <tornadoalert@gmail.com>
2018-01-23 15:15:35 +0530

    first commit

---
+++ preprocess.py
@@ -0,0 +1,25 @@
+from keras.preprocessing.image import ImageDataGenerator
+
+# Data Augmentation and rescale
+train_gen = ImageDataGenerator(
+    rescale=1./255,
+    horizontal_flip=True,
+    rotation_range=40,
+    shear_range=0.2,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    zoom_range=0.3,)
+
+# Only rescale
+test_val_gen = ImageDataGenerator(rescale=1./255)
+
+# Resizing for Xception Net
+train = train_gen.flow_from_directory('data/train', target_size=(299, 299), class_mode='binary')
+val = test_val_gen.flow_from_directory('data/val', target_size=(299, 299), class_mode='binary')
+
+# Total samples/batch_size(32) 
+train_steps = 15
+val_steps = 4
+test_steps = 2
+
+
---
+++ train.py
@@ -0,0 +1,30 @@
+from preprocess import train, val, train_steps, val_steps
+from keras.applications import Xception
+from keras import layers, models
+from keras import callbacks, optimizers
+# Uneven class distribution
+class_weight = {
+    train.class_indices['normal']:1,
+    train.class_indices['glaucoma']: 1.86
+}
+
+# Using pretrained Xception Net as Convolutional feature extractor
+conv_base = Xception(include_top=False,weights='imagenet',input_shape=(299,299,3))
+conv_base.trainable = False
+
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Dense(520, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1,activation='softmax'))
+
+# Output to tensorboard
+tensorboard = callbacks.TensorBoard('/output',1)
+
+model.compile(optimizers.Adam(1e-5),'binary_crossentropy',metrics=['acc'])
+
+# Train model!
+model.fit_generator(train,train_steps,20,callbacks=[tensorboard],validation_data=val,validation_steps=val_steps)
+
+# Save to output
+model.save('/output/model_data.h5')
